c3e1d50ff09f273d4b835b4dfba5b5a627153c83
xandercagexxx <philippe@accorsi.info>
2018-04-26 10:34:53 +0200

    ajout de fichier/dossier

--- view/mainpage.php
+++ view/mainpage.php
@@ -4,17 +4,11 @@
 <head>
   <title>Acceuil</title>
 </head>
-  <h1>Bienvenu sur le forum de discussion</h1>
-  <h2>"""Site en cours de construction"""</h2>
-  <h3>Identification</h3>
-  Name:<input pattern=".{3,255}" TITLE="Veuillez utiliser minimum 3 carractères" type="text"  name="inputName" ><br>
-  Mail:<input pattern=".{6,30}" type="email"  name="inputEmail" ><br>
-  <input type="submit">
-
+<!-- 
 <p>1 - Il faut un liens pour créer un sujet de discussion</p>
 <p>2 - il faut afficher les titres des discussion existante + liens vers la discussion</p>
 <p>3 - il faut 1 liens vers la discussion instantanée</p>
-<p>3 - il faut 1 liens vers les CGU en bas de page</p>
+<p>3 - il faut 1 liens vers les CGU en bas de page</p> -->
 
 
 
---
+++ view/newaccount.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Création de compte</title>
+  </head>
+  <body>
+    <h3>Identification</h3>
+    Name:<input pattern=".{3,255}" TITLE="Veuillez utiliser minimum 3 carractères" type="text"  name="inputName" ><br>
+    
+    <input type="submit">
+
+  </body>
+</html>
