949100c45c5cfa272b61c9df1cada17b99935351
enterovirus <phoenix_fire_@hotmail.fr>
2018-04-26 11:58:23 +0200

    newControllerCGU

--- view/header.php
+++ view/header.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>en-tête</title>
     <h1>Bienvenu sur le forum de discussion</h1>
     <h2>"""Site en cours de construction"""</h2>
-  </head>
-  <body>
-
-  </body>
-</html>
--- view/mainpage.php
+++ view/mainpage.php
@@ -4,13 +4,15 @@
 <head>
   <title>Acceuil</title>
 </head>
-<!-- 
+
+<?php require_once("header.php");?>
+<!--
 <p>1 - Il faut un liens pour créer un sujet de discussion</p>
 <p>2 - il faut afficher les titres des discussion existante + liens vers la discussion</p>
 <p>3 - il faut 1 liens vers la discussion instantanée</p>
 <p>3 - il faut 1 liens vers les CGU en bas de page</p> -->
 
 
-
+<?php require_once("footer.php"); ?>
 </body>
 </html>
