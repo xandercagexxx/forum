de3168c74e977acb5572a4b225e1f981397aa1d7
xandercagexxx <philippe@accorsi.info>
2018-04-27 11:26:25 +0200

    add authenticate

--- view/newaccount.php
+++ view/newaccount.php
@@ -10,7 +10,12 @@
     Mot de passe:<input id="userPassword" type="password" autocomplete="current-password" required minlength="8"
        maxlength="32">
     <input type="submit" value="Créer">
-
+<?php
+$dbconn = pg_connect("host=localhost dbname=forum user=forum password=forum");
+//connexion à la base de données
+$query = "INSERT INTO forum VALUES ('$_POST[inputName]','$_POST[Créer]')";
+$result = pg_query($query);
+ ?>
 
 
   </body>
