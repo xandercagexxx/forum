058d826c0348a99256068a40ed42895d98cc55d7
enterovirus <phoenix_fire_@hotmail.fr>
2018-05-16 10:03:12 +0200

    monComitWoaw

--- view/mainpage.php
+++ view/mainpage.php
@@ -6,12 +6,14 @@
 </head>
 
 <?php require_once("header.php");?>
+<h1>Bienvenu sur le forum de discussion</h1>
 <!--
 <p>1 - Il faut un liens pour créer un sujet de discussion</p>
 <p>2 - il faut afficher les titres des discussion existante + liens vers la discussion</p>
 <p>3 - il faut 1 liens vers la discussion instantanée</p>
 <p>3 - il faut 1 liens vers les CGU en bas de page</p> -->
-
+<h1><a href="index.php?controller=maincontroller&action=showthreads">Forum</a></h1>
+<h1><a href="index.php?controller=maincontroller&action=addthreads">Ajouter un sujet</a></h1>
 
 <?php require_once("footer.php"); ?>
 </body>
